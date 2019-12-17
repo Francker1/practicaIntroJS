1897d7ebdaacbae8d8051ad9e8a4393dd91e295c
Italo Franco <italo.ravina@gmail.com>
2019-12-18 00:23:07 +0100

    kata 2 primer ejercicio a falta de comprobaciones previas para evitar strings o números mayores a 3999

--- kata_2.js
+++ kata_2.js
@@ -8,24 +8,70 @@ const arabToRoman = number => {
 
         if( typeof number == "number" ){
 
-        msg = 'Number Here!!!';
-        console.log( 'MSG: ' + msg );
+            if( number > 1 && number <= 3999){
+                /*my code here*/
+                const numArray = {
+                    M : "1000",
+                    D : "500",
+                    C : "100",
+                    L : "50",
+                    X : "10",
+                    V : "5",
+                    I : "1",
+                };
+
+                let res = [];
+
+                for (var x in numArray) {
+
+                    // console.log(x + ": " + numArray[x])
+                    if( number >= numArray[x]){
+
+                        let numberHight = Math.floor(number / numArray[x]);
+
+                        /*aíslo la mayor cantidad entera*/
+                        number -= (numberHight * numArray[x]);
+
+                        /*no se repitan 3 o más letras*/
+                        if( numberHight <= 3 ){
+                            while(numberHight--){
+
+                                /*voy añadiendo las letras*/
+                                res.push(x);
+                                //console.log(res);
+                            }
+                        }
+
+                    }else{
+                        //console.log('mayor');
+                        res.push('');
+                    }
+
+                }
+
+                msg = res.join('');
+                 console.log( msg );
+
+            }else{
+
+                throw "error2";
+            }
 
         }else{
-            throw "Error";
-        };
-
+            throw "error";
+        }
 
     } catch( err ) {
 
-        if( err == 'Error' ) {
-        
-        msg = 'Introduce a number!!!';
-        console.log( 'MSG: ' + msg);
-
-        }
+        if( err == "error" ) { msg = "Introduce a number!!!"; }
+        if( err == "error2" ) { msg = "Your number must be mayor than 1 and minor than 3999"; }
        
-    } 
-}
+    }finally{
 
-arabToRoman( 12345 );
+        return msg;
+    }
+};
+
+arabToRoman(85);
+arabToRoman(780);
+arabToRoman(2580);
